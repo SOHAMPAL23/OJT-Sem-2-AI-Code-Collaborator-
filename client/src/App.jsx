c24c989d2e70305e3099f0b3baee062b5865eb39
himkar-cmd <himkarsingh312@gmail.com>
2025-05-26 14:01:15 +0530

    quick fix editor page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,27 @@
 import Loginpage from './loginpage.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
+import { ThemeProvider } from './context/ThemeContext';
 
 import DashboardPage from './Dashboard.jsx';
 import EditorPage from './EditorPage.jsx';
 
 const App = () => {
   return (
-        <Routes>
-          <Route path="/" element={<Loginpage />} />
-          <Route 
-            path="/dashboard" 
-            element={
-              <ProtectedRoute>
-                <DashboardPage />
-              </ProtectedRoute>
-            } 
-          />
-          <Route path='/editor' element={<EditorPage/>} />
-        </Routes>
+    <ThemeProvider>
+      <Routes>
+        <Route path="/" element={<Loginpage />} />
+        <Route 
+          path="/dashboard" 
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          } 
+        />
+        <Route path='/editor' element={<EditorPage/>} />
+      </Routes>
+    </ThemeProvider>
   )
 }
 
